6d7af40efc078fca86374325a649797ca5a80998
nikita.mashei <nikita.mashei@semrush.com>
2022-10-26 01:00:36 +0300

    register tool window factory

---
+++ src/main/kotlin/dev/mashei/redisplugin/factory/RedisToolWindowFactory.kt
@@ -0,0 +1,12 @@
+package dev.mashei.redisplugin.factory
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+
+class RedisToolWindowFactory : ToolWindowFactory {
+
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        TODO("Not yet implemented")
+    }
+}
