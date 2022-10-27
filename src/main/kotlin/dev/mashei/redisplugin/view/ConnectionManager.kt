08456d0ef415c227f4dd45871df3f4f98dac2a7b
nikita.mashei <nikita.mashei@semrush.com>
2022-10-27 20:32:21 +0300

    register ConnectionManaget as a service, init tool bar with add action

--- src/main/kotlin/dev/mashei/redisplugin/view/ConnectionManager.kt
+++ src/main/kotlin/dev/mashei/redisplugin/view/ConnectionManager.kt
@@ -1,12 +1,23 @@
 package dev.mashei.redisplugin.view
 
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.ActionToolbar
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.components.Service
 import dev.mashei.redisplugin.action.AddConnectionAction
-import java.util.function.Consumer
+import javax.swing.JPanel
 
+@Service
 class ConnectionManager {
 
-    fun init() {
-        val add = AddConnectionAction()
-        add.actionEventConsumer = Consumer { t -> t }
+    fun createToolBar(connectionPanel: JPanel): ActionToolbar {
+        val actions = DefaultActionGroup()
+        actions.add(AddConnectionAction())
+        actions.addSeparator()
+
+        val toolBar = ActionManager.getInstance().createActionToolbar("ToolwindowToolbar", actions, true)
+        toolBar.targetComponent = connectionPanel
+        toolBar.adjustTheSameSize(true)
+        return toolBar
     }
 }
