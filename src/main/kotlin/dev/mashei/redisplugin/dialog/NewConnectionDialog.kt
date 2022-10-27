5f911c5dc3d5699a3282e9dffb7e452b3c69df7a
nikita.mashei <nik-mashej6@yandex.ru>
2022-10-27 22:30:25 +0300

    change dialog size, fill text fields horizontally

--- src/main/kotlin/dev/mashei/redisplugin/dialog/NewConnectionDialog.kt
+++ src/main/kotlin/dev/mashei/redisplugin/dialog/NewConnectionDialog.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import org.jetbrains.annotations.ApiStatus
 import javax.swing.JComponent
 
@@ -15,10 +16,12 @@ class NewConnectionDialog(project: Project) : DialogWrapper(project) {
         return panel {
             row("Host:") {
                 textField()
+                    .horizontalAlign(HorizontalAlign.FILL)
                     .bindText(connection::host)
             }
             row("Port:") {
                 textField()
+                    .horizontalAlign(HorizontalAlign.FILL)
                     .bindText(connection::port)
             }
         }
@@ -32,7 +35,7 @@ class NewConnectionDialog(project: Project) : DialogWrapper(project) {
 
     init {
         this.title = "Connection Settings"
-        this.setSize(650, 240)
+        this.setSize(375, 240)
         super.init()
     }
 }
